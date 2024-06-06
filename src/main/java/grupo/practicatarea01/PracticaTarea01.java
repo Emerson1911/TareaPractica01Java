50af5564e92b90d03690de37810e1fec612d8337
Emerson1911 <1284662@clases.edu.sv>
2024-06-05 19:21:16 -0600

    Commit 1

---
+++ src/main/java/grupo/practicatarea01/PracticaTarea01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package grupo.practicatarea01;
+
+/**
+ *
+ * @author emers
+ */
+public class PracticaTarea01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
